a0e175d73112e811b0ac3f06a2d8e212a13ffb2c
João Vitor <jvl1@discente.ifpe.edu.br>
2024-11-12 09:10:53 -0300

    Criação e configuração do projeto

---
+++ src/main/java/br/com/zedacarga/zedacarga_api/ZedacargaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zedacarga.zedacarga_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZedacargaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZedacargaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zedacarga/zedacarga_api/ZedacargaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zedacarga.zedacarga_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZedacargaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
