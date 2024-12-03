0d5817df2b2113da19c9880ab87ebff7dde7c2e9
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-12-03 19:49:01 -0300

    ajuste de classe auditavel e negócio

--- src/main/java/br/com/zedacarga/zedacarga_api/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/zedacarga/zedacarga_api/util/entity/EntidadeNegocio.java
@@ -2,15 +2,27 @@ package br.com.zedacarga.zedacarga_api.util.entity;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@SuppressWarnings("serial")
 @Getter
 @Setter
 @EqualsAndHashCode(of = { "id" })
+@MappedSuperclass
 public abstract class EntidadeNegocio implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    @JsonIgnore
+    @Column
     private Boolean habilitado;
-   
-   }
+
+}
