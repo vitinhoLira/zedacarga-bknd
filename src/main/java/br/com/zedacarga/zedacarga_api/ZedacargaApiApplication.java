119aee93b7ab5df828358e621229c6732652cb48
João Vitor <jvl1@discente.ifpe.edu.br>
2024-12-21 14:39:05 -0300

    implementando bd web

--- src/main/java/br/com/zedacarga/zedacarga_api/ZedacargaApiApplication.java
+++ src/main/java/br/com/zedacarga/zedacarga_api/ZedacargaApiApplication.java
@@ -1,13 +1,17 @@
 package br.com.zedacarga.zedacarga_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZedacargaApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZedacargaApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        SpringApplication.run(ZedacargaApiApplication.class, args);
+    }
 }
