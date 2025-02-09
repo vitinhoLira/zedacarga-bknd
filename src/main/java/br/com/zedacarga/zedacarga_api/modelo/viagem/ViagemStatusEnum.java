33836bb087abffa3fd5e2cc4c1a5f9fd62ad44d2
vitinhoLira <contato.joaovitorlira@hotmail.com>
2025-02-09 18:25:58 -0300

    removido status CANCELADO

--- src/main/java/br/com/zedacarga/zedacarga_api/modelo/viagem/ViagemStatusEnum.java
+++ src/main/java/br/com/zedacarga/zedacarga_api/modelo/viagem/ViagemStatusEnum.java
@@ -2,9 +2,8 @@ package br.com.zedacarga.zedacarga_api.modelo.viagem;
 
 public class ViagemStatusEnum {
 
-    public enum StatusViagem {
-      PENDENTE, ACEITO, RECUSADO, ANDAMENTO, CONCLUIDO, CANCELADO
-    }
-  
+  public enum StatusViagem {
+    PENDENTE, ACEITO, RECUSADO, ANDAMENTO, CONCLUIDO
   }
-  
+
+}
